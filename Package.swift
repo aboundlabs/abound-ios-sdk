8d9940298c2301a8a4f88c9e668c60764903a38d
Tim Field <tim@aboundlabs.com>
2021-06-23 15:30:39 -0400

    Version 0.8.0

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Abound",
-            url: "https://sdk.aboundlabs.com/Abound-iOS-SDK-0.8.0-alpha3.zip",
-            checksum: "c8d0c915b5ddcba27c34c66fc6464973d1d12296270f2b6a669abd4aca800441"
+            url: "https://sdk.aboundlabs.com/Abound-iOS-SDK-0.8.0.zip",
+            checksum: "5dc6a1fdfd84fd5ca9ab786f5d22be01ce7c04a414165171716ce914ba11c4ec"
         )
     ]
 )
